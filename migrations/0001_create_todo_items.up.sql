0db0e9323c73d8a1dad28076f7c4efb226ba39ae
Hamed Soleymani <hamedslyn@gmail.com>
2025-09-05 13:24:15 +0330

    [🐛][master] Fix database migration error

--- migrations/0001_create_todo_items.up.sql
+++ migrations/0001_create_todo_items.up.sql
@@ -1,5 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE IF NOT EXISTS todo_items (
     id          uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     description text NOT NULL,
-    due_date    timestamp NOT NULL,
+    due_date    timestamp NOT NULL
 );
